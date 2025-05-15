21c5960576a481d0d4685ee648651c7ec096da01
luan51900761 <luanpro0021@gmail.com>
2025-05-16 00:50:46 +0700

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from '../components/header/header.component';
+import { FooterComponent } from '../components/footer/footer.component';
+import { BodyComponent } from '../components/body/body.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+    standalone: true,
+  imports: [RouterOutlet,HeaderComponent,FooterComponent, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'practive-angular-19';
+  hello = 'hello world by old way';
+  helloSignal = signal('hello world by signal');
+  helloSignalFromChild = signal(0);
+  childSayhello = (event: number) => {
+    this.helloSignalFromChild.set(event);
+  };
 }
